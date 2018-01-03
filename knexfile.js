a16c97bf97ea4173cdf90e7ef597474c9cb0ac22
Robert Blutner <robert@blutner.de>
2018-01-04 00:46:22 +0100

    Database port testing

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,8 @@ module.exports = {
             host: Config.get('database.host'),
             database: Config.get('database.name'),
             user: Config.get('database.user'),
-            password: Config.get('database.password')
+            password: Config.get('database.password'),
+            port: Config.get('database.port')
         }
     }
 }
