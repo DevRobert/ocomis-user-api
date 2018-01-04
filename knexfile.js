09d813616b4e6ac94d44fec956481a1789d2d977
Robert Blutner <robert@blutner.de>
2018-01-04 02:13:01 +0100

    Travis

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,16 @@
 const Config = require('config')
 
-module.exports = {
-    development: {
-        client: 'postgresql',
-        connection: {
-            host: Config.get('database.host'),
-            database: Config.get('database.name'),
-            user: Config.get('database.user'),
-            password: Config.get('database.password'),
-            port: Config.get('database.port')
-        }
+const options = {
+    client: 'postgresql',
+    connection: {
+        host: Config.get('database.host'),
+        database: Config.get('database.name'),
+        user: Config.get('database.user'),
+        password: Config.get('database.password'),
+        port: Config.get('database.port')
     }
+}
+
+module.exports = {
+    test: options
 }
